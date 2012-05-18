ad4f1b23cb0ec426bd388a66d41c76f36d03e88f
Mickaël Menu <mickael.menu@gmail.com>
2012-05-18 18:56:22 +0200

    Added distribute_setup.py to automatically download setuptools if needed.

--- setup.py
+++ setup.py
@@ -15,6 +15,10 @@
 # You should have received a copy of the GNU General Public License along
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 
+# automatically downloads setuptools if needed
+import distribute_setup
+distribute_setup.use_setuptools()
+
 from setuptools import setup, find_packages
 
 setup(
